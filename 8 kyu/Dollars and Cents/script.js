186d97a2bd62d2ea3efff9435f12960507b72144
RedShucov <redshucov@gmail.com>
2023-12-25 23:00:49 +0300

    Will add a solution to the new kata 8 kyu - "Dollars and Cents"

---
+++ 8 kyu/Dollars and Cents/script.js
@@ -0,0 +1,16 @@
+// 25.12.23
+// Kata - 'Do you speak "English"?'
+// https://www.codewars.com/kata/55902c5eaa8069a5b4000083/train/javascript
+
+// Language - JavaScript
+
+// Difficulty - 8 kyu
+
+/**
+ * The function formats the amount into a string with a currency sign.
+ * @param {number} amount - an amount in dollars.
+ * @returns {string}
+ */
+const formatString = (amount) => '$' + amount;
+
+console.log(formatString(39.99));
