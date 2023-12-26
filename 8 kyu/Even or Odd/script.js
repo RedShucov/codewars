ff19500e6b2aec56124eeb6f5070e65e48da9f13
RedShucov <redshucov@gmail.com>
2023-12-26 22:36:52 +0300

    Will add a solution to the new kata 8 kyu - "Even or Odd"

---
+++ 8 kyu/Even or Odd/script.js
@@ -0,0 +1,16 @@
+// 26.12.23
+// Kata - 'Even or Odd'
+// https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
+
+// Language - JavaScript
+
+// Difficulty - 8 kyu
+
+/**
+ * The function checks for an even number or an odd one.
+ * @param {number} num - a number to check.
+ * @returns {boolean}
+ */
+const isOdd = (num) => num % 2 === 0;
+
+console.log(isOdd(1));
