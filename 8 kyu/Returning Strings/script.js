002c6c6851438e3c7efe21900e83d8768e8c6159
RedShucov <redshucov@gmail.com>
2023-12-27 23:47:05 +0300

    Will add a solution to the new kata 8 kyu - "Returning Strings"

---
+++ 8 kyu/Returning Strings/script.js
@@ -0,0 +1,16 @@
+// 27.12.23
+// Kata - 'Returning Strings'
+// https://www.codewars.com/kata/55a70521798b14d4750000a4/train/javascript
+
+// Language - JavaScript
+
+// Difficulty - 8 kyu
+
+/**
+ * The function returns a greeting for the user.
+ * @param {string} name - a name user.
+ * @returns {string}
+ */
+const greet = (name) => {
+  return `Hello, ${name} how are you doing today?`;
+};
